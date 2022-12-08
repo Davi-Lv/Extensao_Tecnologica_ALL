7979727c40ab33c59db3ed77f68ef9ea7861c572
Davi Mordonho <85120918+Davi-Lv@users.noreply.github.com>
2022-12-08 20:00:09 -0300

    INSTALAÇÃO E CONFIGURAÇÃO DE ROTAS

--- primeiroapp/App.js
+++ primeiroapp/App.js
@@ -1,63 +1,19 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Avatar, Button, Input } from 'react-native-elements';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './screens/Login';
+import Cadastro from './screens/Cadastro';
 
-
-const Flex = () => {
+const Stack = createNativeStackNavigator();
+function App() {
   return (
-    <View style={[styles.container, {
-      flexDirection: "column"
-    }]}>
-      <View style={{ flex: 2.5, alignItems: "center", paddingTop: 100 }}>
-        <Avatar
-          size="xlarge"
-          rounded
-          source={{
-            uri:
-              'http://cdn.onlinewebfonts.com/svg/img_329115.png',
-          }}
-        />
-      </View>
-      <View style={{ flex: 2 }}>
-        <Text style={styles.entrarContaText}>Entrar na conta</Text>
-
-        <Input placeholder='Login'
-          leftIcon={{type: 'email', name: 'email', color: '#414E68' }}
-        />
-        <Input placeholder="Senha"
-          secureTextEntry={true}
-          leftIcon={{type:'Password', name: 'vpn-key', color: '#414E68' }}
-        />
-      </View>
-      <View style={{ flex: 3 }}>
-        <Button title="Login"
-          buttonStyle={[styles.Button, {backgroundColor: '#435159'}]}
-        />
-
-        <Button title="Cadastre-se"
-          buttonStyle={[styles.Button]}
-        />
-      </View>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Cadastro" component={Cadastro} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: '#EBF0F2',
-
-  },
-  Button: {
-    marginBottom: 20,
-    backgroundColor: '#697F8C'
-  },
-  entrarContaText: {
-    fontSize: 20,
-    fontWeight: "bold",
-    paddingBottom: 10,
-  }
-});
-
-export default Flex;
+export default App;
---
+++ primeiroapp/screens/Login.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Avatar, Button, Input } from 'react-native-elements';
+
+
+export default function Login({ navigation }) {
+    return (
+        <View style={[styles.container, {
+            flexDirection: "column"
+        }]}>
+            <View style={{ flex: 2.5, alignItems: "center", paddingTop: 100 }}>
+                <Avatar
+                    size="xlarge"
+                    rounded
+                    source={{
+                        uri:
+                            'http://cdn.onlinewebfonts.com/svg/img_329115.png',
+                    }}
+                />
+            </View>
+            <View style={{ flex: 2 }}>
+                <Text style={styles.entrarContaText}>Entrar na conta</Text>
+
+                <Input placeholder='Login'
+                    leftIcon={{ type: 'email', name: 'email', color: '#414E68' }}
+                />
+                <Input placeholder="Senha"
+                    secureTextEntry={true}
+                    leftIcon={{ type: 'Password', name: 'vpn-key', color: '#414E68' }}
+                />
+            </View>
+            <View style={{ flex: 3 }}>
+                <Button title="Login"
+                    buttonStyle={[styles.Button, { backgroundColor: '#435159' }]}
+                />
+
+                <Button title="Cadastre-se"
+                    onPress={() => navigation.navigate('Cadastro')}
+                    buttonStyle={[styles.Button]}
+                />
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#EBF0F2',
+
+    },
+    Button: {
+        marginBottom: 20,
+        backgroundColor: '#697F8C'
+    },
+    entrarContaText: {
+        fontSize: 20,
+        fontWeight: "bold",
+        paddingBottom: 10,
+    }
+});
+
